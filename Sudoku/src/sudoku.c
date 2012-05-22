42616650b3549c29baa3fe649703ff577b0a9669
Malphaet <max.rigaut@gmail.com>
2012-05-22 06:10:10 +0200

    Trying to talktive it

--- Sudoku/src/sudoku.c
+++ Sudoku/src/sudoku.c
@@ -28,40 +28,53 @@
 int main(int nbargs,char **kwargs){
 	char *path;
 	GRILLE grille,gres;
-	infos nfo={0,0};
+	HISTORIQUE current;
+	/*infos nfo={0,0};*/
+	int v;
+	/*ELEMENT historique;*/
 	
 	if (nbargs<2){
-		printf("Le nombre d'arguments est incorrect\n Usage: Backtrack <grille>\n");
+		printf("Le nombre d'arguments est incorrect\n Usage: sudoku <grille>\n");
 		exit(EXIT_FAILURE);
 	}
 
 	path=kwargs[1];
 	init_grille(grille);
+	init_historique(&historique);
+	init_historique(&parametres);
 	saisie_grille(grille,path);
 
 	affiche_grille(grille);
 	
 	
-	printf("%f\n",total_candidats(grille));
+	/*printf("%f\n",total_candidats(grille));*/
 	contrainte_unicite_grille(grille);
-	printf("%f\n",total_candidats(grille));
+	/*printf("%f\n",total_candidats(grille));*/
 	
 	init_grille(gres);
 	
 	backtracking_resolution(&grille,&gres);
-	
 	affiche_grille(grille);
-	affiche_grille_candidats(grille);
+	/*affiche_grille_candidats(grille);*/
+	
 	
 	printf("Solution (Par Backtracking)\n");
 	affiche_grille(gres);
+	/*
 	if (0) {
 		init_grille(gres); init_grille(grille); saisie_grille(grille,path);
 		printf("Infos complementaires :\n");
 		backtracking_infos(&grille,&gres,&nfo);
 		printf("Nb solutions : %d\n",nfo.nb_sols);
 		printf("Nb nodes explorees : %d\n",nfo.depth);
-	}
+	}*/
 	
+	current=historique.suivant;
+	printf("Verbosity (0: None / 1: All) :");
+	scanf("%d",&v);
+	while (current!=NULL){
+		print_element(current,v);
+		current=current->precedent;
+	}
 	return 0;
 }
